f1bcf2fd47df399a2d402914f52dde067cc57c95
liuquanneng <379252771@qq.com>
2018-11-05 03:52:38 +0800

    websocket

--- src/main/java/handler/WebSocketHandler.java
+++ src/main/java/handler/WebSocketHandler.java
@@ -1,4 +1,38 @@
 package handler;
 
-public class WebSocketHandler {
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+public class WebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        Channel channel = ctx.channel();
+        System.out.println("收到消息:"+msg);
+        for(;;){
+
+            channel.writeAndFlush(new TextWebSocketFrame("服务器时间："+LocalDateTime.now()));
+            Thread.sleep(3000);
+        }
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded="+ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved="+ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("发生异常！");
+        ctx.close();
+    }
+
 }
--- src/main/java/server/WebSocketServer.java
+++ src/main/java/server/WebSocketServer.java
@@ -1,4 +1,27 @@
 package server;
 
+import initalizer.WebSocketInitalizer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
 public class WebSocketServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        try {
+            ChannelFuture channelFuture = bootstrap.group(bossGroup, workGroup).handler(new LoggingHandler(LogLevel.INFO)).channel(NioServerSocketChannel.class)
+                    .childHandler(new WebSocketInitalizer()).bind(892).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
 }
